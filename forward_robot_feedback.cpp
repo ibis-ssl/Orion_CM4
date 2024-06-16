20926460d937ecfdab95d149d20b5e4cdc71c799
KANAIHIROYUKI <b1503146@planet.kanazawa-it.ac.jp>
2024-06-16 21:00:16 +0900

    書き直しver

--- forward_robot_feedback.cpp
+++ forward_robot_feedback.cpp
@@ -1,7 +1,4 @@
 #include <arpa/inet.h>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-#include <cstring>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <stdint.h>
@@ -10,8 +7,12 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <termios.h> //ttyパラメータの構造体
+#include <termios.h>  //ttyパラメータの構造体
 #include <unistd.h>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <cstring>
 
 #define SERIAL_PORT "/dev/serial0"
 
@@ -20,34 +21,20 @@ union Data {
   char b[4];
 };
 
-
 /// @brief Different ways a serial port may be flushed.
-enum flush_type
-{
-  flush_receive = TCIFLUSH,
-  flush_send = TCOFLUSH,
-  flush_both = TCIOFLUSH
-};
+enum flush_type { flush_receive = TCIFLUSH, flush_send = TCOFLUSH, flush_both = TCIOFLUSH };
 
-void flush_serial_port(
-  boost::asio::serial_port& serial,
-  flush_type what,
-  boost::system::error_code& error)
+void flush_serial_port(boost::asio::serial_port & serial, flush_type what, boost::system::error_code & error)
 {
-  if (0 == ::tcflush(serial.lowest_layer().native_handle(), what))
-  {
+  if (0 == ::tcflush(serial.lowest_layer().native_handle(), what)) {
     error = boost::system::error_code();
-  }
-  else
-  {
-    error = boost::system::error_code(errno,
-        boost::asio::error::get_system_category());
+  } else {
+    error = boost::system::error_code(errno, boost::asio::error::get_system_category());
   }
 }
 
-int main() {
-
-
+int main()
+{
   printf("start");
 
   int count = 0;
@@ -61,28 +48,25 @@ int main() {
    * シリアル通信の設定
    */
 
-
-  startpoint:
-
-  
+startpoint:
 
   boost::asio::io_service io;
   boost::asio::serial_port serial(io, SERIAL_PORT);
   serial.set_option(boost::asio::serial_port_base::baud_rate(921600));
   serial.set_option(boost::asio::serial_port_base::character_size(8 /* data bits */));
-  serial.set_option(boost::asio::serial_port_base::parity( boost::asio::serial_port_base::parity::none));
+  serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
   serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
   boost::system::error_code error;
 
   flush_serial_port(serial, flush_receive, error);
-  
+
   /**
    * UDP通信の設定
    */
   int sock;
   struct sockaddr_in addr;
   in_addr_t ipaddr;
-  int cnt_nodata=0;
+  int cnt_nodata = 0;
 
   sock = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -91,130 +75,48 @@ int main() {
   addr.sin_addr.s_addr = inet_addr("224.5.20.104");
 
   ipaddr = inet_addr("192.168.20.104");
-  if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF, (char *)&ipaddr,
-                sizeof(ipaddr)) != 0) {
+  if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF, (char *)&ipaddr, sizeof(ipaddr)) != 0) {
     perror("setsockopt");
     return 1;
   }
 
   size_t total_length = 0;
 
-
+  char uart_rx_buf[PACKET_SIZE];
+  uint32_t buf_idx = 0, cycle_div = 0;
 
   while (1) {
-
-    uint8_t start_byte_idx = 0;
     size_t n = serial.read_some(boost::asio::buffer(buf, sizeof(buf)));
-    
-
-    /*if(n==PACKET_SIZE){
-        for (size_t j = 0; j < PACKET_SIZE; j++) {
-          Rxbuf[j]=buf[j];
+    for (int i = 0; i < n; i++) {
+      /*if(buf[i] == '\n'){
+        printf("n = %3d ",n);
       }
-    }*/
+      printf("%c",buf[i]);*/
 
-    if(n==PACKET_SIZE || total_length==PACKET_SIZE){ 
-      
-    flush_serial_port(serial, flush_receive, error);
-    
-    total_length=0;
-    
-    while ((!(Rxbuf[start_byte_idx] == 0xAB &&
-              Rxbuf[start_byte_idx + 1] == 0xEA)) &&
-            start_byte_idx < sizeof(Rxbuf)) {
-      start_byte_idx++;
-    }
-    if (start_byte_idx >= sizeof(Rxbuf)) {
-      for (uint8_t k = 0; k < (sizeof(Rxdata)); k++) {
-        Rxdata[k] = 0;
-      }
-      // 受信なしデータクリア
-    } else {
-      for (uint8_t k = 0; k < sizeof(Rxbuf); k++) {
-        if ((start_byte_idx + k) >= sizeof(Rxbuf)) {
-          Rxdata[k] = Rxbuf[k - (sizeof(Rxbuf) - start_byte_idx)];
+      if (buf[i] == 0xAB && buf_idx == 0) {
+        uart_rx_buf[buf_idx] = buf[i];
+        buf_idx = 1;
+      } else if (buf[i] == 0xEA && buf_idx == 1) {
+        uart_rx_buf[buf_idx] = buf[i];
+        buf_idx = 2;
+      } else if (buf_idx >= 2) {
+        uart_rx_buf[buf_idx] = buf[i];
+        buf_idx++;
+        if (buf_idx >= PACKET_SIZE) {
+          buf_idx = 0;
+          cycle_div++;
         }
-
-        else {
-          Rxdata[k] = Rxbuf[start_byte_idx + k];
-        }
-      }
-    }
-
-    count++;
-
-    if (count > 20) {
-
-      printf(" S_id=%2d", start_byte_idx);
-
-    /* printf(" Read %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x", Rxbuf[0],
-            Rxbuf[1],Rxbuf[2], Rxbuf[3], Rxbuf[4], Rxbuf[5], Rxbuf[6],
-            Rxbuf[7], Rxbuf[8], Rxbuf[9], Rxbuf[10], Rxbuf[11],
-            Rxbuf[12], Rxbuf[13], Rxbuf[14], Rxbuf[15]);*/
-
-      printf(" Data %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x %2x", Rxdata[0],
-            Rxdata[1], Rxdata[2], Rxdata[3], Rxdata[4], Rxdata[5], Rxdata[6],
-            Rxdata[7], Rxdata[8], Rxdata[9], Rxdata[10], Rxdata[11],
-            Rxdata[12], Rxdata[13], Rxdata[14], Rxdata[15]);
-
-      Data yaw;
-      if (Rxdata[2] == 10) {
-        yaw.b[0] = Rxdata[4];
-        yaw.b[1] = Rxdata[5];
-        yaw.b[2] = Rxdata[6];
-        yaw.b[3] = Rxdata[7];
-      }
-
-      printf(" yaw=%5f", yaw.f);
-      printf("\n");
-      fflush(stdout);
-      count = 0;
-    }
-
-    sendto(sock, Rxdata, PACKET_SIZE, 0, (struct sockaddr *)&addr,sizeof(addr));
-        cnt_nodata=0;
-
-  }
-  else{
-
-    for (size_t i = 0; i < n; ++i) {
-				Rxbuf[total_length + i] = buf[i];
-      }
-			total_length += n;
-
-      if(total_length>PACKET_SIZE){
-        total_length=0;
-        for (size_t i = 0; i < PACKET_SIZE; i++) {
-            Rxbuf[i] = 0;
-        }
-      }
-      
-      
-      cnt_nodata++;
-
-      if(cnt_nodata>200){
-          printf("wait data %d \r\n",cnt_nodata);
-          for (size_t i = 0; i < PACKET_SIZE; i++) {
-              Rxbuf[i] = 0;
+        if (cycle_div > 10) {
+          cycle_div = 0;
+          for (int pi = 0; pi < PACKET_SIZE; pi++) {
+            printf("0x%02x ", uart_rx_buf[pi]);
           }
-          cnt_nodata=0;
-          flush_serial_port(serial, flush_receive, error);
-          //serial.cancel();
-          //serial.close();
-          //close(sock);
-          //goto startpoint;
+          printf("\n");
+          sendto(sock, uart_rx_buf, PACKET_SIZE, 0, (struct sockaddr *)&addr, sizeof(addr));
+        }
       }
-
+    }
   }
-
-
-   // printf("length =%2d total_length =%2d\n", n,total_length );
-
-  }
-
-
-
-  close(sock);
 
   return 0;
 }
