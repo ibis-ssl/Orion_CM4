9a7769aa91b00eef1ba26af8e8106d9b0936b92f
cm4 <raspi>
2025-04-07 15:24:53 +0100

    setupスクリプトの追加

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# 移動
+cd "$(dirname "$0")"
+
+SERVICE_NAME=control_server.service
+SERVICE_PATH="/etc/systemd/system/$SERVICE_NAME"
+
+# systemdサービスファイルを配置
+echo "Installing systemd service..."
+sudo cp $SERVICE_NAME $SERVICE_PATH
+
+# systemdにサービスを登録・有効化
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl restart $SERVICE_NAME
+
+echo "Setup complete. Service is now running."
